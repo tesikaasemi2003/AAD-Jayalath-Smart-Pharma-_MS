f563296b481943f2bf5931327c10b09f870dd0dc
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-15 17:58:33 +0530

    first commit

---
+++ src/test/java/lk/ijse/Jayalath_Smart_Pharma/JayalathSmartPharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.Jayalath_Smart_Pharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayalathSmartPharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
