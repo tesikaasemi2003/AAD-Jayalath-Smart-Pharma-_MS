341e55ce96b24da21fdf5dfd43f37d1c7d162954
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-18 22:15:28 +0530

    created Category repository

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.Jayalath_Smart_Pharma.repository;
+
+import lk.ijse.Jayalath_Smart_Pharma.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
