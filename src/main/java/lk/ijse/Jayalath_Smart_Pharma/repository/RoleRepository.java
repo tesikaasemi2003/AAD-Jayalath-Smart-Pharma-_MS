aaa21598006cbd0f76baf304467e7620165c7bdd
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-18 22:13:24 +0530

    created Role repository

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.Jayalath_Smart_Pharma.repository;
+
+import lk.ijse.Jayalath_Smart_Pharma.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
