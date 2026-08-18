6bfb8d60c05af05d8495a4f4effaf38550119fc8
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-18 22:39:10 +0530

    created SalesOrderItem repository

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/SalesOrderItem.java
@@ -0,0 +1,8 @@
+package lk.ijse.Jayalath_Smart_Pharma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SalesOrderItem extends JpaRepository<SalesOrderItem,Long> {
+}
