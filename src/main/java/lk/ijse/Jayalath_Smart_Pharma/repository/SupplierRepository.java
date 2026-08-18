a71f3e4da1dd75d934697e9f846c347736da52b4
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-18 22:21:09 +0530

    created Supplier repository

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.Jayalath_Smart_Pharma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.function.Supplier;
+
+
+public interface SupplierRepository extends JpaRepository<Supplier,Long> {
+}
