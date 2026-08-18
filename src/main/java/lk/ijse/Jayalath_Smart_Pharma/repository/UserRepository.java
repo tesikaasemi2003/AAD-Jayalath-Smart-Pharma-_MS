18fdfa47a2fb15289a1109857207dcf40e6d1f2f
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-18 22:09:59 +0530

    created User repository

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package lk.ijse.Jayalath_Smart_Pharma.repository;
+
+import lk.ijse.Jayalath_Smart_Pharma.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
