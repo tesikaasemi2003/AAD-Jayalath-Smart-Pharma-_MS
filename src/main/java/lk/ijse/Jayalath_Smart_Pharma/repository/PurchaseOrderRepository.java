70d196af7062340a85407c9b11912d1318519f0e
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-18 22:43:23 +0530

    created PurchaseOrder repository

--- src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/PurchaseOrderRepository.java
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/PurchaseOrderRepository.java
@@ -1,4 +1,9 @@
 package lk.ijse.Jayalath_Smart_Pharma.repository;
 
-public interface PurchaseOrderRepository {
+import lk.ijse.Jayalath_Smart_Pharma.entity.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder,Long> {
 }
