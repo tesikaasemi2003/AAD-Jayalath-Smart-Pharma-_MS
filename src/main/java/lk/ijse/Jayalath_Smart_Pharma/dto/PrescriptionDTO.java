aac749796b0fe0183ead74d5c47dd99cfbe3146f
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-19 23:04:30 +0530

    implemented PrescriptionDTO

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/PrescriptionDTO.java
@@ -0,0 +1,29 @@
+package lk.ijse.Jayalath_Smart_Pharma.dto;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lk.ijse.Jayalath_Smart_Pharma.entity.Doctor;
+import lk.ijse.Jayalath_Smart_Pharma.entity.Patient;
+import lk.ijse.Jayalath_Smart_Pharma.entity.PrescriptionItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrescriptionDTO {
+    private long prescriptionId;
+    private Doctor doctor;
+    private Patient patient;
+    private LocalDate issueDate;
+    private String remarks;
+    private List<PrescriptionItem> items ;
+}
