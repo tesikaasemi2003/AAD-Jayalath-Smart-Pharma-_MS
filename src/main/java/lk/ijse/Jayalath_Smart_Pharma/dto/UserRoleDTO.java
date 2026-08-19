042352ed2de47c076e711f8e2e5022468f015d7f
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-19 23:29:51 +0530

    implemented UserRoleDTO

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/UserRoleDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.Jayalath_Smart_Pharma.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleDTO {
+    private Long userId;
+    private Long roleId;
+}
