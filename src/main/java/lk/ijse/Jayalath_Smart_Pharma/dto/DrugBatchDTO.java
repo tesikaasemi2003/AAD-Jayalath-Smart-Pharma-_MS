54df2849eb6feeb59f1bb8c5507ef6169f0208f7
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-19 22:55:24 +0530

    implemented DrugBatchDTO

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/DrugBatchDTO.java
@@ -0,0 +1,27 @@
+package lk.ijse.Jayalath_Smart_Pharma.dto;
+
+import jakarta.persistence.*;
+import lk.ijse.Jayalath_Smart_Pharma.entity.Drug;
+import lk.ijse.Jayalath_Smart_Pharma.entity.Inventory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DrugBatchDTO {
+    private String batchId;
+    private String batchNumber;
+    private LocalDate manufactureDate;
+    private LocalDate expiryDate;
+    private Double purchasePrice;
+    private Double sellingPrice;
+    private Double discountPercentage;
+    private String barcodeString;
+    private Drug drug;
+    private Inventory inventory;
+}
