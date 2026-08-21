e4db18af5cab39eac25877872cf21847467a2bfe
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-21 11:48:03 +0530

    implemented AuthResponseDTO

--- src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/AuthResponseDTO.java
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/AuthResponseDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.Jayalath_Smart_Pharma.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponseDTO {
+    private String token;
+    private String username;
+    private String role;
+    private String fullName;
 }
