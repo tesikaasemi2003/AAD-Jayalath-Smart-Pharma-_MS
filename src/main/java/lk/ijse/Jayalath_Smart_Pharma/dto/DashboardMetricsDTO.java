bc419b4da7fc0302364fea60c0171a50edc43b4a
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-22 19:53:48 +0530

    implemented DashboardMetricsDTO

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/DashboardMetricsDTO.java
@@ -0,0 +1,8 @@
+package lk.ijse.Jayalath_Smart_Pharma.dto;
+
+public class DashboardMetricsDTO {
+    private Double totalDailySales;
+    private Integer lowStockDrugCount;
+    private Integer expiringBatchesCount;
+    private Integer totalPrescriptionsToday;
+}
