22007e47a588a2a45c45308a77d1e61e0b2b7e2b
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-22 13:14:46 +0530

    implemented GoogleAuthRequestDTO

---
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/dto/GoogleAuthRequestDTO.java
@@ -0,0 +1,12 @@
+package lk.ijse.Jayalath_Smart_Pharma.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoogleAuthRequestDTO {
+    private String googleToken;
+}
--- src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/DrugBatchRepository.java
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/repository/DrugBatchRepository.java
@@ -2,8 +2,18 @@ package lk.ijse.Jayalath_Smart_Pharma.repository;
 
 import lk.ijse.Jayalath_Smart_Pharma.entity.DrugBatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DrugBatchRepository extends JpaRepository<DrugBatch,Long> {
+
+    @Query("SELECT b FROM DrugBatch b JOIN Inventory i ON b.batchId = i.drugBatch.batchId " +
+            "WHERE b.drug.drugId = :drugId AND i.quantityOnHand > 0 AND b.expiryDate > :today " +
+            "ORDER BY b.expiryDate ASC")
+    List<DrugBatch> findAvailableBatchesByFefo(@Param("drugId") Long drugId, @Param("today") LocalDate today);
 }
