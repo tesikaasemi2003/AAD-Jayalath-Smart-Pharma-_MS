ce5176314986fa276d00e336b133ffbbfe222f43
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-23 21:04:40 +0530

    implemented GET mapping in RoleController

--- src/main/java/lk/ijse/Jayalath_Smart_Pharma/controller/RoleController.java
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/controller/RoleController.java
@@ -3,10 +3,9 @@ package lk.ijse.Jayalath_Smart_Pharma.controller;
 import lk.ijse.Jayalath_Smart_Pharma.constant.CommonResponse;
 import lk.ijse.Jayalath_Smart_Pharma.dto.RoleDTO;
 import lk.ijse.Jayalath_Smart_Pharma.service.RoleService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static lk.ijse.Jayalath_Smart_Pharma.constant.ResponseCode.OPERATION_SUCCESS;
 import static lk.ijse.Jayalath_Smart_Pharma.constant.ResponseMessage.SUCCESS_MESSAGE;
@@ -24,5 +23,11 @@ public class RoleController {
     public CommonResponse saveRole(@RequestBody RoleDTO roleDTO) {
         roleService.saveRole(roleDTO);
         return new CommonResponse(OPERATION_SUCCESS, SUCCESS_MESSAGE);
+    }
+
+    @GetMapping
+    public CommonResponse getAllRoles() {
+        List<RoleDTO> roleList = roleService.getAllRoles();
+        return new CommonResponse(OPERATION_SUCCESS, SUCCESS_MESSAGE, roleList);
     }
 }
