2a4f667923980b57108a5a8098335b4980fbd5c5
tesikaasemi2003 <asemitesika68@gmail.com>
2026-08-23 21:02:41 +0530

    implemented getAllRoles in RoleServiceImpl

--- src/main/java/lk/ijse/Jayalath_Smart_Pharma/service/Impl/RoleServiceImpl.java
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/service/Impl/RoleServiceImpl.java
@@ -8,6 +8,9 @@ import lk.ijse.Jayalath_Smart_Pharma.service.RoleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Slf4j
 public class RoleServiceImpl implements RoleService {
@@ -26,6 +29,24 @@ public class RoleServiceImpl implements RoleService {
             roleRepository.save(role);
         }catch (Exception e){
             log.error("Error in saveRole method" + e.getMessage());
+            throw e;
+        }
+    }
+
+    public List<RoleDTO> getAllRoles() {
+        log.info("Executing getAllRoles method");
+        try {
+            List<Role> roles = roleRepository.findAll();
+            List<RoleDTO> roleDTOs = new ArrayList<>();
+            for (Role role : roles) {
+                RoleDTO roleDTO = new RoleDTO();
+                roleDTO.setRoleId(role.getRoleId());
+                roleDTO.setRoleName(role.getRoleName());
+                roleDTOs.add(roleDTO);
+            }
+            return roleDTOs;
+        }catch(Exception e){
+            log.error("Error in getAllRoles method" + e.getMessage());
             throw e;
         }
     }
--- src/main/java/lk/ijse/Jayalath_Smart_Pharma/service/RoleService.java
+++ src/main/java/lk/ijse/Jayalath_Smart_Pharma/service/RoleService.java
@@ -2,6 +2,9 @@ package lk.ijse.Jayalath_Smart_Pharma.service;
 
 import lk.ijse.Jayalath_Smart_Pharma.dto.RoleDTO;
 
+import java.util.List;
+
 public interface RoleService {
     public void saveRole(RoleDTO roleDTO);
+    public List<RoleDTO> getAllRoles();
 }
